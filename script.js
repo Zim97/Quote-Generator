5a0c007898400ea7ababca8a3c9f9e9378832713
Zim97 <112665921+Zim97@users.noreply.github.com>
2023-11-29 21:32:15 -0500

    adding a loader

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-const quoteContainer = document.getElementById("quote-conatiner");
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("Twitter");
 const newQuoteBtn = document.getElementById("new-quote");
-("https://twitter.com/intent/tweet");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function completeLoading() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 //Show New Quote
-function showNewQuote() {
+function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   authorText.textContent = quote.author;
   quoteText.textContent = quote.text;
+  completeLoading();
 }
-// showNewQuote();
 
 //Get Quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    showNewQuote();
+    newQuote();
   } catch (error) {
     //Error Handling
+    alert("failed to fetch new Quote");
+    completeLoading();
   }
 }
 
@@ -32,7 +46,7 @@ function tweetQuote() {
   window.open(twitterUrl, "_blank");
 }
 
-newQuoteBtn.addEventListener("click", showNewQuote);
+newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
 //onLoad
